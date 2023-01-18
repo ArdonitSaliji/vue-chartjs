5f8abb0208cf9be3151da58acd56c203c63dfd64
Ardonit Saliji <ardonit.1980@gmail.com>
2023-01-18 19:57:13 +0100

    init

---
+++ App.vue
@@ -0,0 +1,13 @@
+<script setup>
+// import { ref } from "vue";
+import ChartComponent from "./components/ChartComponent.vue";
+</script>
+
+<template>
+  <div>
+    <div>
+      <canvas id="myChart"> </canvas>
+      <ChartComponent />
+    </div>
+  </div>
+</template>
